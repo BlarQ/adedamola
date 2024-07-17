a6f431bc7eb0cbc5c734173fe6dd1eb98b846e23
BlarQ <collinsogunlala@gmail.com>
2024-07-17 18:27:47 +0100

    feat: about page 70% done

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,15 +1,18 @@
+'use client'
+import Image from 'next/image'
 import React from 'react'
+import { motion } from 'framer-motion'
 
 export default function About() {
   return (
-    <div className='min-h-[100vh] px-7 sm:px-28 py-20 sm:py-24' id='about'>
+    <div className='min-h-[100vh] px-7 sm:px-28 py-20 sm:py-24 overflow-x-hidden' id='about'>
       <div className='flex flex-col items-end'>
 
         <h1 className='text-xl sm:text-3xl font-bold'>About Me</h1>
         <hr className='border-4 w-14 sm:w-20 border-[#4AC8A3] right-0' />
       </div>
 
-      <div className='flex flex-col sm:flex-row sm:items-center justify-center w-full'>
+      <div className='flex flex-col-reverse sm:flex-row sm:items-center justify-center w-full pt-10 sm:pt-0 gap-10'>
         <div className='sm:w-1/2'>
 
           <p className='py-4'>Dear Viewer,</p>
@@ -19,8 +22,17 @@ export default function About() {
 
           <p className='py-4'>Thanks, <br />Adedamola.</p>
         </div>
-        <div className='sm:w-1/2 flex items-center justify-center'>
-          <p>holla!</p>
+        <div className='sm:w-1/2 flex items-center justify-center max-h-screen'>
+
+          <motion.div 
+          initial={{ x: '10%', opacity: 0}}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1.5 }}
+          viewport={{ once: false, amount: 0.5 }}
+          className='min-h-full'>
+            <Image src='/about.svg' className='rounded-br-full rounded-tl-full rounded-bl-full w-full shadow-xl' alt='Hands on Deck' title='hands on deck' width={500} height={500}/>
+
+          </motion.div>
         </div>
 
       </div>
