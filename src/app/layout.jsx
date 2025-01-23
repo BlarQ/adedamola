1e3e79820b66d2702233d66dc9c3cee6854e0725
BlarQ <collinsogunlala@gmail.com>
2025-01-23 15:06:21 +0100

    feat: added speed insight

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({ children }) {
 
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
